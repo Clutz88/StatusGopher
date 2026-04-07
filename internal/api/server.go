c44fbb90b16c8767cfee39c70e707e6ca0bd326b
Chris Cutts <christopher.cutts@yourparkingspace.co.uk>
2026-04-07 16:31:53 +0100

    Fix new main files

--- internal/api/server.go
+++ internal/api/server.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"context"
+	"log"
 	"net/http"
 
 	"github.com/Clutz88/StatusGopher/internal/database"
@@ -21,4 +23,18 @@ func NewServer(addr string, db *database.DB) *Server {
 	}
 
 	return s
+}
+
+func (s *Server) Start() error {
+	log.Printf("API server listening on %s", s.httpServer.Addr)
+	if err := s.httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Server) Stop(ctx context.Context) error {
+	log.Println("API server shutting down...")
+	return s.httpServer.Shutdown(ctx)
 }
