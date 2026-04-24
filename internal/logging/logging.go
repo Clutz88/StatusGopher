3bbcb93d8110e8fc04470df20e6b0ebbffabf897
Chris Cutts <christopher.cutts@yourparkingspace.co.uk>
2026-04-24 15:15:21 +0100

    Fix style issues

--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -6,14 +6,17 @@ import (
 	"os"
 )
 
+// LoggerKey struct for logging
 type LoggerKey struct{}
 
+// New returns a new JSON slog logger
 func New(level slog.Level) *slog.Logger {
 	h := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: level})
 
 	return slog.New(h)
 }
 
+// FromCtx builds a slog logger containing the ctx to be able to locate logs across a single request
 func FromCtx(ctx context.Context) *slog.Logger {
 	if l, ok := ctx.Value(LoggerKey{}).(*slog.Logger); ok {
 		return l
